85704c819f6679ff3fdbbe71dda96b6f1991c30c
Sandeepkulkarni77 <sandeepkulkarni2003@gmail.com>
2025-11-23 21:35:49 +0530

    updated the postgres task

--- db/task4.sql
+++ db/task4.sql
@@ -1,4 +1,4 @@
-vboxuser@sandeep-vm1:~/zero-to-prod-uptime-an-observability-stack-/db$ sudo -u postgres psql 
+ivboxuser@sandeep-vm1:~/zero-to-prod-uptime-an-observability-stack-/db$ sudo -u postgres psql 
 [sudo] password for vboxuser: 
 could not change directory to "/home/vboxuser/zero-to-prod-uptime-an-observability-stack-/db": Permission denied
 psql (14.19 (Ubuntu 14.19-0ubuntu0.22.04.1))
@@ -6,13 +6,6 @@ Type "help" for help.
 
 postgres=# CREATE DATABASE uptime; 
 CREATE DATABASE
-postgres=# CREATE USER app.user WITH PASSWORD 'user123'; 
-ERROR:  syntax error at or near "."
-LINE 1: CREATE USER app.user WITH PASSWORD 'user123';
-                       ^
-postgres=# CREATE USER app.user WITH PASSWORD 'user@123'; 
-ERROR:  syntax error at or near "."
-LINE 1: CREATE USER app.user WITH PASSWORD 'user@123';
                        ^
 postgres=# CREATE USER app_user WITH PASSWORD 'user@123'; 
 CREATE ROLE
@@ -23,12 +16,7 @@ postgres=# CREATE TABLE hc_metrics (
     status INT,
     latency_ms INT
 ) PARTITION BY RANGE (ts);
-CREATE TABLE
-postgres=# CREATE TABLE hc_metrics_2025_11 PARTITION OF hc_metrics
-FOR VALUES FROM '2025-11-01 00:00:00' TO '2025-12-01 00:00:00';
-ERROR:  syntax error at or near "'2025-11-01 00:00:00'"
-LINE 2: FOR VALUES FROM '2025-11-01 00:00:00' TO '2025-12-01 00:00:0...
-                        ^
+CREATE TABLE                        ^
 postgres=# CREATE TABLE hc_metrics_2025_11 PARTITION OF hc_metrics
 FOR VALUES FROM ('2025-11-01 00:00:00') TO ('2025-12-01 00:00:00');
 CREATE TABLE
@@ -43,3 +31,4 @@ postgres=# GRANT INSERT ON hc_metrics_2025_12 TO app_user;
 GRANT
 postgres=# CREATE INDEX idx_hc_metrics_ts_route ON hc_metrics (ts, route);
 CREATE INDEX
+
