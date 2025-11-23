776e24aefd31edd3e225e859f6946343741d80c0
Sandeepkulkarni77 <sandeepkulkarni2003@gmail.com>
2025-11-23 21:45:36 +0530

    Postgres schema and partitions created

--- db/task4.sql
+++ db/task4.sql
@@ -1,30 +1,23 @@
 ivboxuser@sandeep-vm1:~/zero-to-prod-uptime-an-observability-stack-/db$ sudo -u postgres psql 
 
-postgres=# CREATE DATABASE uptime; 
-CREATE DATABASE
-                       ^
-postgres=# CREATE USER app_user WITH PASSWORD 'user@123'; 
-CREATE ROLE
-postgres=# CREATE TABLE hc_metrics (
+postgres=# CREATE DATABASE uptCREATE DATABASE uptime;
+
+CREATE USER app_user WITH PASSWORD 'user@123';
+
+CREATE TABLE hc_metrics (
     ts TIMESTAMPTZ,
     host TEXT,
     route TEXT,
     status INT,
     latency_ms INT
 ) PARTITION BY RANGE (ts);
-CREATE TABLE                        ^
-postgres=# CREATE TABLE hc_metrics_2025_11 PARTITION OF hc_metrics
+
+CREATE TABLE hc_metrics_2025_11 PARTITION OF hc_metrics
 FOR VALUES FROM ('2025-11-01 00:00:00') TO ('2025-12-01 00:00:00');
-CREATE TABLE
-postgres=# GRANT SELECT, INSERT ON hc_metrics TO app_user;
-GRANT
-postgres=# GRANT INSERT ON hc_metrics_2025_11 TO app_user;
-GRANT
-postgres=# CREATE TABLE hc_metrics_2025_12 PARTITION OF hc_metrics
+
+GRANT SELECT, INSERT ON hc_metrics TO app_user;
+GRANT INSERT ON hc_metrics_2025_11 TO app_user;
+
+CREATE TABLE hc_metrics_2025_12 PARTITION OF hc_metrics
 FOR VALUES FROM ('2025-12-01 00:00:00') TO ('2026-01-01 00:00:00');
-CREATE TABLE
-postgres=# GRANT INSERT ON hc_metrics_2025_12 TO app_user;
-GRANT
-postgres=# CREATE INDEX idx_hc_metrics_ts_route ON hc_metrics (ts, route);
-CREATE INDEX
 
